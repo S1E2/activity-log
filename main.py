9e28b2a6be54c6828e1c811ad462a0100e69158b
S1E2 <hmahmood@protonmail.com>
2018-07-17 19:47:40 +0100

    Initial commit:
    - tkinter frame switching function implemented ('base')
    - placeholder login screen ('StartPage')
    - initial homescreen ('HomeScreen')
    - initial commit to 'today' tracking feature ('today')
    - initial commit to 'weekly' tracking feature ('weekly')

---
+++ main.py
@@ -0,0 +1,125 @@
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/
+
+import tkinter as tk
+from datetime import datetime
+
+class base(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, HomeScreen, today, weekly, bulk):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.intro_label = tk.Label(self,text="Welcome to the program")
+        self.intro_label.pack()
+
+        self.v = tk.StringVar(self, value="Enter Username")
+        self.user_text = tk.Entry(self, textvariable=self.v)
+        self.user_text.pack()
+
+        self.c = tk.StringVar(self, value="Enter Password")
+        self.pass_text = tk.Entry(self, textvariable=self.c)
+        self.pass_text.pack()
+
+        self.login_button = tk.Button(self,text="Login", command=self.login_check)
+        self.login_button.pack()
+
+    def login_check(self):
+        if 2 > 1:
+            self.login_success = tk.Label(self,text="Successful login, continue to the home screen.")
+            self.login_success.pack()
+            self.login_proceed = tk.Button(self, text="Proceed", command=lambda: self.controller.show_frame(HomeScreen))
+            self.login_proceed.pack()
+
+
+class HomeScreen(tk.Frame):
+
+    def __init__(self, parent, controller):
+        self.controller=controller
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Homescreen")
+        label.pack(pady=10, padx=10)
+
+        self.today_label = tk.Button(self,text="Today", command=lambda: self.controller.show_frame(today))
+        self.today_label.pack()
+
+        self.week_label = tk.Button(self,text="Weekly", command=lambda: self.controller.show_frame(weekly))
+        self.week_label.pack()
+
+class today(tk.Frame):
+
+    def __init__(self, parent, controller):
+        self.controller = controller
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Today's Progress")
+        label.pack(pady=10, padx=10)
+
+        self.current_label = tk.Label(self,text="The current hour is (24 hour layout):")
+        self.current_label.pack()
+        variable = datetime.now().strftime('%H:00')
+        self.current_hour = tk.Label(self,text=variable)
+        self.current_hour.pack()
+
+        self.question_label = tk.Label(self, text="What are you working on?")
+        self.question_label.pack()
+
+        self.descriptive_label = tk.Label(self, text="Descriptive description of your current hour")
+        self.descriptive_label.pack()
+
+        self.back_homescreen = tk.Button(self, text="Go back", command=lambda: self.controller.show_frame(HomeScreen))
+        self.back_homescreen.pack()
+
+        self.bulk_fill = tk.Button(self, text="Bulk fill hours", command=lambda: self.controller.show_frame(bulk))
+        self.bulk_fill.pack()
+
+class bulk(tk.Frame):
+
+    def __init__(self, parent, controller):
+        self.controller = controller
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Bulk fill your hours")
+        label.pack(pady=10, padx=10)
+
+        self.back_today = tk.Button(self, text="Go back", command=lambda: self.controller.show_frame(today))
+        self.back_today.pack()
+
+class weekly(tk.Frame):
+
+    def __init__(self, parent, controller):
+        self.controller = controller
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Weekly Progress")
+        label.pack(pady=10, padx=10)
+
+        self.back_homescreen = tk.Button(self, text="Go back", command=lambda: self.controller.show_frame(HomeScreen))
+        self.back_homescreen.pack()
+
+
+app = base()
+app.mainloop()
