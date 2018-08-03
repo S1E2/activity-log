154663b39046de56462d80b9fb2897aee8fb5a29
S1E2 <hmahmood@protonmail.com>
2018-08-03 20:06:23 +0100

    v1.0: simplified program, bulk fill (for today's date) only. Interacts with main.text fine.
    Signed-off-by: S1E2 <hmahmood@protonmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# The code for changing pages was derived from:
-# http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
-# License: http://creativecommons.org/licenses/by-sa/3.0/
-
 import tkinter as tk
 from datetime import datetime
+import sys
+
+LARGE_FONT = ("Verdana", 12)
 
 
 class Base(tk.Tk):
@@ -11,217 +10,139 @@ class Base(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        menubar = MenuBar(self)
+        self.config(menu=menubar)
+
         self.frames = {}
-
-        for F in (StartPage, HomeScreen, Today, Select, Bulk, ):
+        for F in (Initialize, Choose, Edit):
             frame = F(container, self)
-
             self.frames[F] = frame
-
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame(StartPage)
+        self.show_frame(Initialize)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 
-class StartPage(tk.Frame):
+class MenuBar(tk.Menu):
 
-    program_list = []
+    def __init__(self, parent):
+        tk.Menu.__init__(self, parent)
+        fileMenu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label="Program", underline=0, menu=fileMenu)
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", underline=1, command=self.quit)
+
+    def quit(self):
+        sys.exit(0)
+
+
+class Initialize(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
         self.controller = controller
-        self.intro_label = tk.Label(self, text="Welcome to the program")
-        self.intro_label.pack(pady=10, padx=10)
-        self.login_button = tk.Button(self, text="Initialize", command=self.load_file)
-        self.login_button.pack()
+        global program_list
+        program_list = []
+        label = tk.Label(self, text="Init Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button = tk.Button(self, text="Initialize", command=self.load_file)
+        button.pack()
 
     def load_file(self):
         # Load the contents of the main.text file into an list (main) for it to be used in the program
         with open('main.text', 'rt') as in_file:  # Open file lorem.txt for reading of text data.
             for line in in_file:  # For each line of text store in a string variable named "line", and
-                self.program_list.append(line)  # add that line to our list of lines.
-        print("Text file initialized. The list in memory is:\n", self.program_list)
-        self.controller.show_frame(HomeScreen)
+                program_list.append(line)  # add that line to our list of lines.
+        print("Text file initialized. The list in memory is:\n", program_list)
+        self.controller.show_frame(Choose)
 
 
-class HomeScreen(StartPage):
+class Choose(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
         self.controller = controller
-        self.date_today = None
-        self.first_date_count = None
-        self.last_date_count = None
-        self.start_append = None
-        self.end_append = None
-        self.first_status = None
-        self.last_status = None
+        label = tk.Label(self, text="Date Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button1 = tk.Button(self, text="Today", command=self.load_today)
+        button1.pack()
+        # TODO : add previous dates functionality
 
-        self.home_title = tk.Label(self, text="Home Screen")
-        self.home_title.pack(pady=10, padx=10)
-        self.today_path = tk.Button(self, text="Today", command=self.combined)
-        self.today_path.pack()
-
-    def combined(self):
-        self.date_check()
-        self.date_status()
-
-    def date_check(self):
-        # check if today's date is in the list. If: continue. If not: create a new pair of dates in the list
-
+    def load_today(self):
         self.date_today = datetime.now().strftime('%d-%m-%Y')
         self.start_append = "{}start\n".format(self.date_today)
         self.end_append = "{}end\n".format(self.date_today)
 
-        for self.count, self.elem in enumerate(self.program_list):
+        for self.count, self.elem in enumerate(program_list):
+
             if self.elem == self.start_append:
-                self.first_status = 1
-                self.first_date_count = self.count
-                print("Found the first date index:", self.first_date_count)
+                global first_date_count
+                first_date_count = self.count
+                print("Found the first date index:", first_date_count)
+
             if self.elem == self.end_append:
-                self.last_status = 1
-                self.last_date_count = self.count
-                print("Found the last date index:", self.last_date_count)
-                print("Both indexes found!!!")
+                global last_date_count
+                last_date_count = self.count
+                print("Found the last date index:", last_date_count)
+                print("Both indexes found, sending you to the next stage.")
+                global temporary_list
+                temporary_list = program_list[first_date_count + 1:last_date_count]
+                self.controller.show_frame(Edit)
                 break
+
             else:
                 print("Date not found on this particular index, trying again.")
         else:
             print("Assuming that there isn't yet the entries for today's date in the list")
             print("Appending the entries for today's date now.")
-            self.program_list.append(self.start_append)
-            self.program_list.append(self.end_append)
-            label = tk.Label(self, text="Today's date which was previously not found, "
-                                        "has now been appended to the list. Please click below to save to the main file"
-                                        "again and start from the StartPage.")
+            self.date_append = [self.start_append, '00:00\n', 'type:empty\n', '01:00\n', 'type:empty\n', '02:00\n',
+                                'type:empty\n', '03:00\n', 'type:empty\n', '04:00\n', 'type:empty\n', '05:00\n',
+                                'type:empty\n', '06:00\n', 'type:empty\n', '07:00\n', 'type:empty\n', '08:00\n',
+                                'type:empty\n', '09:00\n', 'type:empty\n', '10:00\n', 'type:empty\n', '11:00\n',
+                                'type:empty\n', '12:00\n', 'type:empty\n', '13:00\n', 'type:empty\n', '14:00\n',
+                                'type:empty\n', '15:00\n', 'type:empty\n', '16:00\n', 'type:empty\n', '17:00\n',
+                                'type:empty\n', '18:00\n', 'type:empty\n', '19:00\n', 'type:empty\n', '20:00\n',
+                                'type:empty\n', '21:00\n', 'type:empty\n', '22:00\n', 'type:empty\n', '23:00\n',
+                                'type:empty\n', self.end_append]
+
+            for i in self.date_append:
+                program_list.append(i)
+            label = tk.Label(self, text="New date appended. Proceed to Edit page.\n")
+            print(program_list)
             label.pack()
-            self.restart_button = tk.Button(self, text="Restart", command=self.restart_save)
+            self.restart_button = tk.Button(self, text="Continue", command=self.save_continue)
             self.restart_button.pack()
 
-        return self.first_date_count, self.last_date_count
-
-    def date_status(self):
-        # this was made since the return (on line 113) ended up showing the frame of TodaySelect for some reason while
-        #  I was on the Today screen. I don't know if the solution is the best as the for loop gets executed again since
-        #  I needed to call the first and last date variables on line 187 (using '()' which calls it). Who knows.
-        if self.first_status and self.last_status == 1:
-            print("Good to go.")
-            self.controller.show_frame(Select)
-
-    def restart_save(self):
-        self.save()
-        self.controller.show_frame(Today)
-
-    def save(self):
+    def save_continue(self):
         print("Attempting save now")
         with open('main.text', 'w') as file:
-            for list_value in self.program_list:
+            for list_value in program_list:
                 file.write(list_value)
-        print(self.program_list)
+        print("Text file initialized. The list in memory is:\n", program_list)
+        self.controller.show_frame(Edit)
 
 
-class Select(HomeScreen):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-
-        self.intro_label = tk.Label(self, text="What would you like to do?")
-        self.intro_label.pack()
-        self.current_button = tk.Button(self, text="Fill in the current hour",
-                                        command=lambda: self.controller.show_frame(Today))
-        self.current_button.pack()
-        self.bulk_button = tk.Button(self, text="Bulk fill the hours of the day",
-                                     command=lambda: self.controller.show_frame(Bulk))
-        self.bulk_button.pack()
-
-
-class Today(HomeScreen):
+class Edit(tk.Frame):
 
     def __init__(self, parent, controller):
-        self.controller = controller
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Today's Progress")
+        label = tk.Label(self, text="Edit Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
-
-        self.current_label = tk.Label(self, text="The current hour is (24hr layout):")
-        self.current_label.pack()
-        self.hour_time = datetime.now().strftime('%H:00\n')
-        self.current_hour = tk.Label(self, text=self.hour_time)
-        self.current_hour.pack()
-
-        self.question_label = tk.Label(self, text="What are you working on?")
-        self.question_label.pack()
 
         # insert type of work (from a list)
         self.OPTIONS = ['Blank(0)', 'Sleep(1)', 'University/Self-study(2)', 'Downtime(3)', 'Examinations(4)',
                         'Social(5)', 'Exercise(6)', 'Productivity(7)', 'Gaming(8)', 'Preparation(9)', 'Family(10)',
                         'Travel(11)', 'Unproductive(12)', 'Paid Work(13)', 'Work Experience(14)']
-
-        self.variable = tk.StringVar()
-        self.variable.set(self.OPTIONS[7])
-        self.type_work = tk.OptionMenu(self, self.variable, *self.OPTIONS)
-        self.type_work.pack()
-        self.submit_hour = tk.Button(self, text="Submit", command=self.append_hour)
-        self.submit_hour.pack()
-        self.save_button = tk.Button(self, text="Save", command=self.save)
-        self.save_button.pack()
-        self.back_home = tk.Button(self, text="Go back", command=lambda: self.controller.show_frame(Select))
-        self.back_home.pack()
-
-    def append_hour(self):
-        self.first_date_count, self.last_date_count = self.date_check()
-        print("\nFirst date pos\n", self.first_date_count)
-        print("\nLast date pos\n", self.last_date_count)
-
-        for count, enum in enumerate(self.OPTIONS):
-            if enum == self.variable.get():
-                self.optioncount = count    # keep the index of the hour type I clicked on to append later
-                break
-
-        # A temporary list is created as we cannot append to a specific index inside a slice. Temp list assumes that
-        #  all of its list contents are that of the contents between today's date markers
-        self.temporary_list = (self.program_list[self.first_date_count + 1:self.last_date_count])
-        print(self.temporary_list)
-
-        for count, enum in enumerate(self.temporary_list):
-            if enum == self.hour_time:
-                # the hour type next to the hour (in terms of index)
-                #  now equals the option (hour type) that the user clicked on
-                self.temporary_list[count+1] = ('%s\n' % self.optioncount)
-                print("Now includes the new appended hour", self.temporary_list)
-                break
-
-        # Deleting the contents of today's date in the main list in order to append the new temporary (updated) list.
-        del self.program_list[self.first_date_count + 1:self.last_date_count]
-
-        # The main list now equals the main list up to the point of the start marker for today's date,
-        # the contents of today's list and the rest of the main lists contents past the end marker (first date + 1).
-        self.program_list = self.program_list[:self.first_date_count + 1] + self.temporary_list + \
-            self.program_list[self.first_date_count+1:]
-
-
-class Bulk(HomeScreen):
-
-    def __init__(self, parent, controller):
-        self.controller = controller
-        tk.Frame.__init__(self, parent)
-        self.label = tk.Label(self, text="Welcome to da bulk page. Useful for checking in your sleep hours")
-        self.label.pack()
-
-        self.label = tk.Label(self, text="Which hours do you wish to fill in?")
-        self.label.pack()
+        self.default = tk.StringVar()
+        self.default.set(self.OPTIONS[7])
 
         self.zerozerovar = tk.IntVar()
         self.zeroonevar = tk.IntVar()
@@ -296,12 +217,92 @@ class Bulk(HomeScreen):
         self.twotwo_check.pack()
         self.twothree_check = tk.Checkbutton(self, text="23:00", variable=self.twothreevar)
         self.twothree_check.pack()
+        self.type_work = tk.OptionMenu(self, self.default, *self.OPTIONS)
+        self.type_work.pack()
+        button1 = tk.Button(self, text="Submit", command=self.append_stageone)
+        button1.pack()
+        button2 = tk.Button(self, text="Back to Choose", command=lambda: controller.show_frame(Choose))
+        button2.pack()
+        button3 = tk.Button(self, text="Print", command=self.test)
+        button3.pack()
+        button4 = tk.Button(self, text="Save", command=self.save_button)
+        button4.pack()
 
-        self.bulk_submit = tk.Button(self, text="Submit", command=self.bulk_append)
-        self.bulk_submit.pack()
+    def append_stageone(self):
+        self.var_counter = [self.zerozerovar.get(), self.zeroonevar.get(), self.zerotwovar.get(),
+                            self.zerothreevar.get(), self.zerofourvar.get(), self.zerofivevar.get(),
+                            self.zerosixvar.get(), self.zerosevenvar.get(), self.zeroeightvar.get(),
+                            self.zeroninevar.get(), self.onezerovar.get(), self.oneonevar.get(),
+                            self.onetwovar.get(), self.onethreevar.get(), self.onefourvar.get(),
+                            self.onefivevar.get(), self.onesixvar.get(), self.onesevenvar.get(),
+                            self.oneeightvar.get(), self.oneninevar.get(), self.twozerovar.get(),
+                            self.twoonevar.get(), self.twotwovar.get(), self.twothreevar.get()]
+        # List containing the state of the checkbuttons on the screen.
 
-    def bulk_append(self):
-        print("!")
+        self.zerotonine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.tentotwentythree = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+        for self.count, self.enum in enumerate(self.OPTIONS):
+            if self.enum == self.default.get():  # Iterate through hour type array to find the hour type I selected
+                self.bulkoption = self.count  # keep the index of the hour type I clicked on to bulk append later
+                print("GOOD")
+                self.append_stagetwo()
+                break   # break as we only need to find the hour type once (one type to fill across a number of hrs)
+
+    def append_stagetwo(self):
+        print("exec stage_two")
+        for self.countcontinue, self.enumcontinue in enumerate(self.var_counter):
+            if self.enumcontinue == 1:   # Checks if a checkbox is ticked
+                if self.countcontinue in self.zerotonine:
+                    print("From 0 to 9")
+                    self.hour_append = ('0%s:00\n' % self.countcontinue)  # we keep the index of the checkbox plus
+                    #  '0:00\n' (the number of indexes in the var_counter array are the same as the number of hours
+                    #  in the day - 23 (Start at 0) ~ 24 hrs) to find it in the temporary list later.
+                    #  Add a '0' behind the string so it mimics the 24 hour format.
+                    print(self.hour_append)
+                    self.append_stagethree()
+                if self.countcontinue in self.tentotwentythree:
+                    print("From 10 to 23.")
+                    self.hour_append = ('%s:00\n' % self.countcontinue)  # we keep the index of the checkbox
+                    #  plus ':00\n' (the number of indexes in the var_counter array are the same as the number
+                    #  of hours in the day - 23 (Start at 0) ~ 24 ) to find it in the temporary list later.
+                    print(self.hour_append)
+                    self.append_stagethree()
+
+    def append_stagethree(self):
+        print("exec stage_three")
+        for self.countfinal, self.enumfinal in enumerate(temporary_list):
+            if self.enumfinal == self.hour_append:
+                temporary_list[self.countfinal+1] = ('%s\n' % self.bulkoption)    # the hour type next to the hour
+                # (in terms of index) now equals the option (hour type) that the user clicked on (tkinter OptionMenu)
+                print("Done-----")
+                break
+        else:
+            print("Nah. ")
+
+    def save_button(self):
+        print("Saving the contents of the temporary list into main list.")
+        global program_list
+
+        # Deleting the contents of today's date in the main list in order
+        # to append the new temporary (updated) list.
+        del program_list[first_date_count + 1:last_date_count]
+
+        # The main list now equals the main list up to the point of the start marker for today's date,
+        # the contents of today's list and the rest of the main lists contents past the end marker (first date + 1).
+        program_list = program_list[:first_date_count + 1] + temporary_list + \
+            program_list[first_date_count + 1:]
+
+        with open('main.text', 'w') as file:
+            for i in program_list:
+                file.write(i)
+
+        print("The following has been written into the text file", program_list)
+
+    def test(self):
+        print("Temporary list\n", temporary_list)
+        print("Main list\n", program_list)
+
 
 app = Base()
 app.mainloop()
